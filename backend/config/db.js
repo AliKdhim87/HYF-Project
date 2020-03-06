6d518e82dee669d68d9c5fcb30a9a3ba2fed0205
AliKadhim87 <alikadhim87nl@gmail.com>
2020-03-06 13:41:33 +0100

    make it better

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-// const config = require("config");
-// const db = config.get("mongoURI");
-const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-m2y4d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// // const config = require("config");
+// // const db = config.get("mongoURI");
+// const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-m2y4d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    });
-    console.log("mongoDB Connected");
-  } catch (error) {
-    console.error(e.message);
-    process.exit(1);
-  }
-};
-module.exports = connectDB;
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(db, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true
+//     });
+//     console.log("mongoDB Connected");
+//   } catch (error) {
+//     console.error(e.message);
+//     process.exit(1);
+//   }
+// };
+// module.exports = connectDB;
